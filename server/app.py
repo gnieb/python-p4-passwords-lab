ecaff0cc71f0f71903e876e59260d29d8e4dc9e4
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-22 16:39:32 -0400

    routes for sign up, check session, log in, and log out

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 
 from config import app, db, api
@@ -16,18 +16,57 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        if username and password:
+
+            new_user = User(username=username)
+            new_user.password_hash = password
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            return make_response(new_user.to_dict(), 201)
+        return make_response({"error":"422 Unprocessable entity"}, 422)
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if not user:
+            return make_response({}, 204)
+        
+        return make_response(user.to_dict(), 200)
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 200) 
+        
+        return make_response({"error":"Unauthorized"}, 401)
 
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        session['user_id'] = None
+        return make_response({}, 204)
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
